3dff634b724db6027fcb63256b9d027c1d6433c3
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-12-12 23:43:20 +0530

    code-added

--- src/app.js
+++ src/app.js
@@ -1,18 +1,76 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const marioModel = require('./models/marioChar');
+const marioModel = require("./models/marioChar");
 
 // Middlewares
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // your code goes here
 
+app.get("/mario", (req, res) => {
+  marioModel.find().then((m) => res.send(m));
+});
 
+app.get("/mario/:id", (req, res) => {
+  const id = req.params.id;
+  marioModel
+    .findById(id)
+    .then((mario) => res.send(mario))
+    .catch((error) => res.status(400).send({ message: error.message }));
+  return;
+});
+
+app.post("/mario", (req, res) => {
+  const mr = new marioModel({
+    name: req.body.name,
+    weight: req.body.weight
+  });
+  mr.save().then((mario) => {
+    if (!mario.name) {
+      res.status(400).send("Name is missing");
+      return;
+    } else if (!mario.weight) {
+      res.status(400).send("Weight is missing");
+    }
+    res.status(201).send(mario);
+  });
+});
+
+/*app.put('/mario/:id',(req,res)=>{
+  const id = req.params.id;
+  marioModel.findByIdAndUpdate(id,req.body,{new:true,upsert:true}).then((result)=>res.send(result)).
+  catch(err=>res.status(400).send({message:err.message}));
+});*/
+
+app.patch("/mario/:id", (req, res) => {
+  const id = req.params.id;
+  marioModel
+    .findByIdAndUpdate(id, req.body, { new: true })
+    .then((result) => res.send(result))
+    .catch((err) => res.status(400).send({ message: err.message }));
+});
+
+app.delete("/mario/:id", (req, res) => {
+  const id = req.params.id;
+  marioModel
+    .findByIdAndDelete(id)
+    .then(() => res.status(200).send("character deleted"))
+    .catch((err) => res.status(400).send({ message: err.message }));
+  /*
+  marioModel.findByIdAndDelete(id,function(result){
+      if(!result){
+          res.status(404).send("Movie not found");
+          return;
+      }
+      res.send(result);
+  });
+  */
+});
 
 module.exports = app;
--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -1,6 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //  Your code goes here
+const marioSchema = new mongoose.Schema({
+  name: String,
+  weight: Number
+});
 
+const marioModel = mongoose.model("marioModel", marioSchema);
 
 module.exports = marioModel;
